ee7916192e0bf8a3e4baa72d7cf22821784ac4cc
Melvil Donnart <melvildonnart3@gmail.com>
2018-03-28 22:17:07 +0200

    start link facade to controller

--- src/view/AcceptRefuseApplicationController.java
+++ src/view/AcceptRefuseApplicationController.java
@@ -4,15 +4,21 @@ import javafx.scene.control.TableView;
 
 
 import javafx.fxml.FXML;
+import facade.ApplicationFacade;
+import facade.UserFacade;
 
 public class AcceptRefuseApplicationController {
 	@FXML
-	TableView universityTableView;
+	TableView<?> universityTableView;
+	protected static ApplicationFacade applicationFacade;
 	/**
      * The constructor.
      * The constructor is called before the initialize() method.
      */
     public AcceptRefuseApplicationController() {
+	 applicationFacade = new ApplicationFacade();
+
+    	applicationFacade.addStatusApplication(1,true);
     }
     
     /**
