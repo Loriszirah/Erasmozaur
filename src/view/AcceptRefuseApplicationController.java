3a03efc122ad244293409bdf6b468fb2789b5b80
Melvil Donnart <melvildonnart3@gmail.com>
2018-03-28 20:24:57 +0200

    Controller AJoutRefuseApp

---
+++ src/view/AcceptRefuseApplicationController.java
@@ -0,0 +1,27 @@
+package view;
+
+import javafx.scene.control.TableView;
+
+
+import javafx.fxml.FXML;
+
+public class AcceptRefuseApplicationController {
+	@FXML
+	TableView universityTableView;
+	/**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public AcceptRefuseApplicationController() {
+    }
+    
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    //getAllAppliersForTheUniversity
+    }
+}
+
