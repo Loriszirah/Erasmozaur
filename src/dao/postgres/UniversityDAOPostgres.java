8934e86f20a598fc667fd9927f33965a59a19b7d
Godefroi Roussel <godefroi.roussel@gmail.com>
2018-03-29 18:26:27 +0200

    Fix : SQL request

--- src/dao/postgres/UniversityDAOPostgres.java
+++ src/dao/postgres/UniversityDAOPostgres.java
@@ -151,10 +151,10 @@ public class UniversityDAOPostgres implements UniversityDAO {
 			Statement state = conn.createStatement();
 			// Check if the username already exist
 
-			ResultSet exists = state.executeQuery("SELECT id_university, name nameUniversity, Cities.name cityName, Countries.name countryName "
+			ResultSet exists = state.executeQuery("SELECT id_university, Universities.name nameUniversity, Cities.name cityName, Countries.name countryName "
 					+ "FROM Universities, Cities, Countries "
 					+ "WHERE Universities.id_city = Cities.id_city "
-					+ "AND Cities.id_country == Countries.id_country ;");
+					+ "AND Cities.id_country = Countries.id_country ;");
 
 			int id_university;
 			String nameUniversity, nameCity, nameCountry;
